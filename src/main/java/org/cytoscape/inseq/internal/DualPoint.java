a01231309cfb4ed7f1259e8273abf6003c16d2b5
John Salamon <salamonj9@gmail.com>
2016-05-19 09:35:55 +0930

    colours

---
+++ src/main/java/org/cytoscape/inseq/internal/DualPoint.java
@@ -0,0 +1,15 @@
+package org.cytoscape.inseq.internal;
+
+import java.awt.geom.Point2D;
+
+public class DualPoint
+{
+	public Point2D.Double p1;
+	public Point2D.Double p2;
+
+	public DualPoint(Point2D.Double p1, Point2D.Double p2)
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+}
