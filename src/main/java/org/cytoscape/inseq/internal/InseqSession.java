c276f1aa31821964789d04be2682fede78c5c39d
jrs <jrs@coptinet.com>
2016-10-06 22:57:57 +1030

    even faster switches

--- src/main/java/org/cytoscape/inseq/internal/InseqSession.java
+++ src/main/java/org/cytoscape/inseq/internal/InseqSession.java
@@ -245,9 +245,11 @@ public class InseqSession {
 
         if(CAA.getCyApplicationManager().getCurrentNetworkView() != n.getView()) 
         {
-            CAA.getCyNetworkViewManager().addNetworkView(n.getView());
+			if(!CAA.getCyNetworkViewManager().viewExists(n.getNetwork())) {
+            	CAA.getCyNetworkViewManager().addNetworkView(n.getView());
+			}
             //CAA.getCyEventHelper().flushPayloadEvents(); 
-            //CAA.getCyApplicationManager().setCurrentNetworkView(n.getView());
+            CAA.getCyApplicationManager().setCurrentNetworkView(n.getView());
         }
     }
     
